4f65b630ecfd8e496fefde0dabe61ebaab3f0330
PSEH-7 <39406614+PSEH-7@users.noreply.github.com>
2020-01-25 16:17:39 +0530

    Add files via upload

--- whether/src/main/java/com/sapient/whether/controller/WeatherApiController.java
+++ whether/src/main/java/com/sapient/whether/controller/WeatherApiController.java
@@ -2,16 +2,23 @@ package com.sapient.whether.controller;
 
 import com.sapient.whether.model.*;
 import com.sapient.whether.service.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/weather")
 public class WeatherApiController {
+
+    static final Logger LOGGER = LoggerFactory.getLogger(WeatherApiController.class.getName());
 
     private final WeatherService weatherService;
 
@@ -28,43 +35,74 @@ public class WeatherApiController {
     @RequestMapping("/threeday/{country}/{city}")
     public Object getWeatherForecast(@PathVariable String country,
                                      @PathVariable String city) {
-        WeatherForecast forecast = this.weatherService.getWeatherForecast(country, city);
         WhetherResponse response = new WhetherResponse();
-        Temperature temperature = new Temperature();
-        List<WeatherEntry> entries = forecast.getEntries();
-        List<Double> tempList = new ArrayList<>();
+        try {
+            WeatherForecast forecast = this.weatherService.getWeatherForecast(country, city);
 
-        Double celsiusTempConversion = 273.0;
+            Temperature temperature = new Temperature();
+            List<WeatherEntry> entries = forecast.getEntries();
+            List<Double> tempList = new ArrayList<>();
 
-        boolean isSunDay = false;
-        boolean isRainDay = false;
-        for (WeatherEntry weatherEntry : entries) {
-            tempList.add(weatherEntry.getTemperature());
-            if (weatherEntry.getTemperature() - celsiusTempConversion > 40) {
-                isSunDay = true;
+            Double celsiusTempConversion = 273.0;
+
+            Date currentDate = new Date();
+            // convert date to calendar
+            Calendar c = Calendar.getInstance();
+            c.setTime(currentDate);
+            // manipulate date
+            c.add(Calendar.DATE, 3); //same with c.add(Calendar.DAY_OF_MONTH, 1);
+            // convert calendar to date
+            Long threeDayTime = c.getTime().getTime();
+
+            Instant time = Instant.ofEpochMilli(threeDayTime * 1000);
+
+            boolean isSunDay = false;
+            boolean isRainDay = false;
+            for (WeatherEntry weatherEntry : entries) {
+                tempList.add(weatherEntry.getTemperature());
+                if (weatherEntry.getTemperature() - celsiusTempConversion > 40) {
+                    isSunDay = true;
+                }
+
+                if (weatherEntry.getRain() != null) {
+                    isRainDay = true;
+                }
+            }
+            if (tempList != null && !tempList.isEmpty()) {
+                tempList.sort(Double::compareTo);
+                temperature.setLowTemperature(tempList.get(0));
+                temperature.setMaxTemperature(tempList.get(tempList.size() - 1));
             }
 
-            if (weatherEntry.getRain() != null) {
-                isRainDay = true;
+            // check for rain season
+            if (isRainDay) {
+                response.setPrediction("Rain is predicted");
+                response.setMessage("Carry umbrella'.");
+            } else if (isSunDay) {
+                response.setPrediction("Temperatures is  above 40 degree celsius.");
+                response.setMessage("Use sunscreen lotion");
+            } else {
+                response.setPrediction("Sunny day.");
+                response.setMessage("'Enjoy the today's whether.");
             }
+            response.setTemperature(temperature);
+        } catch (Exception ex) {
+            LOGGER.error("Error occurred while checking the whether prediction.");
+            response.setMessage("Error occurred while checking the whether prediction.");
         }
-        tempList.sort(Double::compareTo);
-        temperature.setLowTemperature(tempList.get(0));
-        temperature.setMaxTemperature(tempList.get(tempList.size() - 1));
-
-        // check for rain season
-        if (isRainDay) {
-            response.setPrediction("Rain is predicted");
-            response.setMessage("Carry umbrella'.");
-        } else if (isSunDay) {
-            response.setPrediction("Temperatures is  above 40 degree celsius.");
-            response.setMessage("Use sunscreen lotion");
-        } else {
-            response.setPrediction("Sunny day.");
-            response.setMessage("'Enjoy the today's whether.");
-        }
-        response.setTemperature(temperature);
         return response;
+    }
+
+    private static void nextThreeDate() {
+        Date currentDate = new Date();
+        // convert date to calendar
+        Calendar c = Calendar.getInstance();
+        c.setTime(currentDate);
+        // manipulate date
+        c.add(Calendar.DATE, 3); //same with c.add(Calendar.DAY_OF_MONTH, 1);
+        // convert calendar to date
+        Long threeDayTime = c.getTime().getTime();
+        System.out.println(c.getTime().getTime());
     }
 
 }
